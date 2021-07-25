cc66c2cd88ec3da64caf7fb32b674470656e4471
Anatoliy <tolik.zhilko@gmail.com>
2021-07-25 22:00:54 +0300

    Completed login form

--- src/main/java/org/example/student_hostel/config/WebSecurityConfig.java
+++ src/main/java/org/example/student_hostel/config/WebSecurityConfig.java
@@ -2,62 +2,42 @@ package org.example.student_hostel.config;
 
 import org.example.student_hostel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
-@EnableWebMvc
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
+    @Autowired
     private UserService userService;
-    private MessageDigestPasswordEncoder passwordEncoder;
-
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
-    @Bean
-    public MessageDigestPasswordEncoder getPasswordEncoder() {
-        return new MessageDigestPasswordEncoder("MD5");
-    }
-
-    @Autowired
-    public void setPasswordEncoder(MessageDigestPasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
-
-
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
-
-                .and()
+                    .antMatchers("/", "/registration", "/static/**", "/activate/*").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
-
-                .and()
-                .logout()
-                .permitAll();
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .rememberMe()
+                    .and()
+                    .logout()
+                    .permitAll();
     }
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
+
+
 }
---
+++ src/main/java/org/example/student_hostel/service/UserService.java
@@ -0,0 +1,30 @@
+package org.example.student_hostel.service;
+
+import org.example.student_hostel.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+
+    private UserRepo userRepo;
+
+    @Autowired
+    public void setUserRepo(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws BadCredentialsException {
+        UserDetails user = userRepo.findByUsername(username);
+        if (user == null) {
+            throw new BadCredentialsException("Користувача не знайдено");
+        }
+
+        return userRepo.findByUsername(username);
+    }
+}
